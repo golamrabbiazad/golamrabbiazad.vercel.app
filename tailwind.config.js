4dd442c81345fbf631185c4111e4bd1b3120f270
Golamrabbi Azad <35021384+golamrabbiazad@users.noreply.github.com>
2022-09-09 12:10:21 +0600

    replace purge to content tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const { spacing, fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./pages/**/*.tsx", "./components/**/*.tsx", "./layouts/**/*.tsx"],
+  content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
