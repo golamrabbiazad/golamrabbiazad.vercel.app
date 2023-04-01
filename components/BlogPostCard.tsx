307816c4cf4da8cabe210f2314bc2c02179c3f7e
Golamrabbi Azad <35021384+golamrabbiazad@users.noreply.github.com>
2023-04-02 03:31:01 +0600

    feat: random post views added

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -46,6 +46,7 @@ export default function BlogPostCard({
               d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
             />
           </svg>
+          {Math.floor(Math.random() * 1000)}
         </div>
       </div>
     </Link>
