f3d39996e4362e4657a65de0bb5ff43f7e379687
golamrabbiazad <golamrabbiazad@gmail.com>
2023-06-01 03:11:43 +0600

    chore: bug fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { withContentlayer } = require("next-contentlayer");
 
+/** @type {import('next').NextConfig} */
 module.exports = withContentlayer({
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: true
 });
