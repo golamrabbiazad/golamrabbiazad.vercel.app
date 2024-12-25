a0214ea340be340cf320942e61d90e31b6253fb7
Golamrabbi Azad <golamrabbiazad@gmail.com>
2024-12-25 07:58:34 +0600

    fix: work updated

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -11,49 +11,37 @@ export default function Work() {
       <div className="grid max-w-6xl grid-cols-1 gap-5 py-16 lg:grid-cols-2">
         <div className="flex flex-col">
           <h2 className="text-xl font-bold lg:mb-2">Current Job</h2>
-          {/* <p>null</p> */}
-          <p className="text-xl">
-            <ExternalLink href="https://www.m4yours.com/" className="font-bold">
+          <p>null</p>
+          {/* <p className="text-lg">
+            <ExternalLink href="https://www.m4yours.com/" className="font-bold underline decoration-2 decoration-blue-500 decoration-clone underline-offset-4">
               M4yours
             </ExternalLink>
             ,<span className="font-medium tracking-tighter"> Senior SWE</span>
           </p>
           <p className="text-sm">From Oct, 2024 - Nov, 2024 <br />(2 month - Contract)</p>
 
-          <p className="text-sm">Elephant Road, Bangladesh</p>
+          <p className="text-sm">Elephant Road, Bangladesh</p> */}
         </div>
 
         <div className="flex flex-col gap-4">
           <h2 className="text-xl font-bold">Previous Jobs</h2>
+
           <div className="mb-2 flex flex-col">
             <p className="text-medium">
               <ExternalLink
                 href="https://www.m4yours.com/"
-                className="font-bold"
+                className="font-bold underline decoration-2 decoration-blue-500 decoration-clone underline-offset-4"
               >
                 M4yours
               </ExternalLink>
-              , <span className="font-medium">Software Engineer II</span>
+              , <span className="font-medium">Software Engineer</span>
             </p>
-            <p className="text-sm">From Jan, 2024 - June, 2024 (6 months)</p>
+            <p className="text-sm">From Jan, 2023 - Nov, 2024 (1 year, 9 months)</p>
             <p className="text-sm">Elephant Road, Bangladesh</p>
           </div>
           <div className="mb-2 flex flex-col">
             <p className="text-medium">
-              <ExternalLink
-                href="https://www.m4yours.com/"
-                className="font-bold"
-              >
-                M4yours
-              </ExternalLink>
-              , <span className="font-medium">Software Engineer I</span>
-            </p>
-            <p className="text-sm">From Jan, 2023 - Dec, 2023 (1 year)</p>
-            <p className="text-sm">Elephant Road, Bangladesh</p>
-          </div>
-          <div className="mb-2 flex flex-col">
-            <p className="text-medium">
-              <ExternalLink href="https://linkedin.com/" className="font-bold">
+              <ExternalLink href="https://linkedin.com/" className="font-bold underline decoration-2 decoration-blue-500 decoration-clone underline-offset-4">
                 Remote
               </ExternalLink>
               , <span className="font-medium">Software Engineer</span>
@@ -65,7 +53,7 @@ export default function Work() {
             <p className="text-medium">
               <ExternalLink
                 href="https://nextsolutionlab.com/"
-                className="font-bold"
+                className="font-bold underline decoration-2 decoration-blue-500 decoration-clone underline-offset-4"
               >
                 Next Solution Lab
               </ExternalLink>
